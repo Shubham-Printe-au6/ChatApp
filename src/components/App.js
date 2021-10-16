12df7b9dc404d99a6cf2e2cbc15858c69adc202c
Shubham Printe <shubham.printe@dogratech.com>
2021-10-17 00:02:31 +0530

    added login form to verify different users simultaneously

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import {ChatEngine} from 'react-chat-engine';
 import ChatFeed from './ChatFeed';
 import './App.css';
+import LoginForm from './LoginForm';
 
 const App = () => {
+
+    const userName = localStorage.getItem('username');
+    const userSecret = localStorage.getItem('password');
+
+    if(!localStorage.getItem('username')) 
+        return <LoginForm />
+
     return ( 
         <ChatEngine
             height="100vh"
             projectID="2ef01646-f820-4dea-8779-dd7e8dc31155"
-            userName="fullmetal"
-            userSecret="123123"
+            userName={userName}
+            userSecret={userSecret}
             renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
          />
     )
---
+++ src/components/LoginForm.js
@@ -0,0 +1,61 @@
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
+const LoginForm = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+
+        
+        const authObject = {
+            'Project-ID': '2ef01646-f820-4dea-8779-dd7e8dc31155',
+            'User-Name': username,
+            'User-Secret': password
+        }
+
+        try{
+            // username|password => chatengine -> give messages
+            await axios.get('https://api.chatengine.io/chats', {
+                headers: authObject
+            });
+            // works out -> logged in successfully
+            localStorage.setItem('username', username);
+            localStorage.setItem('password', password);
+
+            // we do a reload to see if the login was successful or not. 
+            // if it was we go to chatengine component
+            // if not we go back to login component
+
+            window.location.reload();
+
+        }catch(err) {
+            // does not work out -> try with different credentials
+            setError('Oops, incorrect credentials');
+        }
+
+    }
+
+    return ( 
+        <div className='wrapper'>
+            <div className='form'>
+                <h1 className='title'>Chat Application</h1>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className='input' placeholder="username" required />
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className='input' placeholder="password" required />
+                    <div align="center">
+                        <button className='button' type='submit'>
+                            <span>Start Chatting</span>
+                        </button>
+                    </div>
+                    <h2 className='error'>
+                        {error}
+                    </h2>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default LoginForm;
